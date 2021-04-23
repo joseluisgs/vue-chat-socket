0737fcdd2f4ed332c811fc42f0adb8d9a4d0422a
joseluisgs <joseluisgonsan@gmail.com>
2021-04-23 20:11:06 +0200

    Servidor con socketio

--- servidor/src/index.ts
+++ servidor/src/index.ts
@@ -1,5 +1,6 @@
-import * as http from "http";
-import chalk from "chalk";
+import * as http from 'http';
+import chalk from 'chalk';
+import io from './io';
 
 // Constantes del servidor
 const HOST = 'localhost';
@@ -7,10 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 // Creamos el servidor
 const httpServer = http.createServer();
+// Le asignamos el socket y su funcionalidad
+io(httpServer);
 
 httpServer.listen(PORT, () => {
   console.log(chalk.green.bold(`🟢 Servidor CHAT escuchando ✅ -> http://${HOST}:${PORT}`));
 });
-
-
-
