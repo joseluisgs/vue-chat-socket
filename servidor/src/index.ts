9910371084b1e1b86d4c8bd661552bae1678a669
joseluisgs <joseluisgonsan@gmail.com>
2021-04-23 19:20:11 +0200

    Servidor web

--- servidor/src/index.ts
+++ servidor/src/index.ts
@@ -1,3 +1,16 @@
-console.log('Hola');
+import * as http from "http";
+import chalk from "chalk";
 
-console.log('chat');
+// Constantes del servidor
+const HOST = 'localhost';
+const PORT = process.env.PORT || 3000;
+
+// Creamos el servidor
+const httpServer = http.createServer();
+
+httpServer.listen(PORT, () => {
+  console.log(chalk.green.bold(`🟢 Servidor CHAT escuchando ✅ -> http://${HOST}:${PORT}`));
+});
+
+
+
