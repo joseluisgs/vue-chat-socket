537ee6838f99102d498519a96ba2323e1223a919
joseluisgs <joseluisgonsan@gmail.com>
2021-04-24 12:47:42 +0200

    Bulma y Buefy

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,14 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './App.vue';
+import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
+
+// Importamos nuestros estilos globales de bulma
+require('./assets/css/main.scss');
+
+Vue.use(Buefy);
 
 new Vue({
   render: (h) => h(App),
