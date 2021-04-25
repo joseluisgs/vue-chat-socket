77029a867a05ebda96066d8b31ade938750a12bd
joseluisgs <joseluisgonsan@gmail.com>
2021-04-25 10:15:20 +0200

    Comentarios

--- server/src/store/SesionStore.ts
+++ server/src/store/SesionStore.ts
@@ -2,10 +2,22 @@
 import ISession from '../interfaces/ISession';
 
 interface SessionStore {
+  /**
+   * Encuetra la sesión dado el id de la misma
+   * @param id ID Sesion
+   */
   findSession(id: string): ISession | undefined;
 
+  /**
+   * Salva una sesión en almacenamiento
+   * @param id ID Sesión
+   * @param session Sesión
+   */
   saveSession(id: string, session: ISession): Map<string, ISession>;
 
+  /**
+   * Obtiene las sesiones existentes
+   */
   findAllSessions(): ISession[];
 }
 
