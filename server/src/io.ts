39c6103c5fc830635b053a017dcda6061412442f
joseluisgs <joseluisgonsan@gmail.com>
2021-04-26 18:40:41 +0200

    Socket Redis

--- server/src/io.ts
+++ server/src/io.ts
@@ -8,26 +8,40 @@ import * as socketio from 'socket.io';
 import http from 'http';
 import chalk from 'chalk';
 import crypto from 'crypto';
-import InMemorySessionStore from './store/InMemorySessionStore';
-import InMemoryMessageStore from './store/InMemoryMessageStore';
+import Redis from 'ioredis';
+import RedisSessionStore from './store/RedisSessionStore';
+import RedisMessageStore from './store/RedisMessageStore';
 import ISession from './interfaces/ISession';
 import IMessage from './interfaces/IMessage';
 
+// Servidor http para worker
+const servicio = http.createServer();
+// Cliente de Redis
+const redisClient = new Redis();
+// Worker
+const { setupWorker } = require('@socket.io/sticky');
+
 // Para obtener ID aleatorios
 const randomId = () => crypto.randomBytes(8).toString('hex');
+
 // Las Sesiones
-const sessionStore = new InMemorySessionStore();
+const sessionStore = new RedisSessionStore(redisClient);
 // Los mensajes
-const messageStore = new InMemoryMessageStore();
+const messageStore = new RedisMessageStore(redisClient);
 
 // Creamos el módulo de configurar. Es una función que recibe Up
-export default (servicio: http.Server) => {
+export default () => {
   // Creamos el socket asociado a nuestro servicio. Configuramos los cors, por si acaso
   const io = new socketio.Server();
   io.attach(servicio, {
     cors: {
       origin: 'http://localhost:8080',
     },
+    // eslint-disable-next-line global-require
+    adapter: require('socket.io-redis')({
+      pubClient: redisClient,
+      subClient: redisClient.duplicate(),
+    }),
   });
 
   // Crea el middleware a usar
@@ -56,7 +70,7 @@ export default (servicio: http.Server) => {
   });
 
   // Si nos llega el evento de connexión
-  io.on('connection', (socket: any) => {
+  io.on('connection', async (socket: any) => {
     socket.emit('status', '👋 Hola desde el servidor');
 
     // Almacenamos la sesion
@@ -64,7 +78,6 @@ export default (servicio: http.Server) => {
       userID: socket.userID,
       username: socket.username,
       connected: true,
-      messages: [],
     });
 
     // Emitimos los datos de la sesion
@@ -76,25 +89,27 @@ export default (servicio: http.Server) => {
     // Nos unimos a la sala del userID
     socket.join(socket.userID);
 
-    // Recuperamos los mensajes de cada usuario y hacemos una cache de ellos
-    const messagesPerUser: Map<string, IMessage[]> = new Map();
-    messageStore.findMessagesForUser(socket.userID).forEach((message) => {
+    // Recogemos todos los mensajes y sesiones de Redis
+    const users: any = [];
+    const [messages, sessions] = await Promise.all([
+      messageStore.findMessagesForUser(socket.userID),
+      sessionStore.findAllSessions(),
+    ]);
+
+    // Sacamos los mensajes por usuario
+    const messagesPerUser = new Map();
+    messages.forEach((message: IMessage) => {
       const { from, to } = message;
-      // Obtenemos los mensajes y analizamos si van o entran de otro usuario
       const otherUser = socket.userID === from ? to : from;
       if (messagesPerUser.has(otherUser)) {
-        // Si existe, lo recuperamos del otro usuario y lo almacenamos en los nuestros
-        messagesPerUser.get(otherUser)?.push(message);
+        messagesPerUser.get(otherUser).push(message);
       } else {
-        // si no es nuestro se lo metemos al otro usuario
         messagesPerUser.set(otherUser, [message]);
       }
     });
 
-    // Lista de usuarios desde la sesion.
-    // Cargamos las sesiones y le pasamos sus mensajes
-    const users: ISession[] = [];
-    sessionStore.findAllSessions().forEach((session) => {
+    // Por cada sesion, creamos los usuarios
+    sessions.forEach((session: ISession) => {
       users.push({
         userID: session.userID,
         username: session.username,
@@ -102,6 +117,8 @@ export default (servicio: http.Server) => {
         messages: messagesPerUser.get(session.userID) || [],
       });
     });
+
+    // Emitimos los usuarios
     socket.emit('users', users);
 
     // Notoficamos los usuarios existentes
@@ -138,10 +155,11 @@ export default (servicio: http.Server) => {
           userID: socket.userID,
           username: socket.username,
           connected: false,
-          messages: [],
         });
         console.log(chalk.yellow(`<- Cliente ${socket.username} desconectado: ${new Date().toLocaleString()}`));
       }
     });
   });
+  // Lanzo el worker con el Socket
+  setupWorker(io);
 };
